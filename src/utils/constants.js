32238b7e5145f0a3ab526c2e22352528dcead8b5
Michelle Jdia <mj.sy.ru@gmail.com>
2021-04-17 21:26:40 +0300

    added consts

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -53,16 +53,16 @@ export const profileJob = document.querySelector('.profile__subtitle');
 export const openBtn = document.querySelector('.profile__button-edit');
 export const openBtnSelector = '.profile__button-edit';
 export const openBtnCreat = document.querySelector('.profile__button-add');
+export const openPopupAvatar = document.querySelector('.profile__hover-edit');
 
 // popup \\
 export const overlay = document.querySelectorAll('.popup');
-
+export const jobInput = document.querySelector('.popup__input-text_type_job');
 export const popUpProfile = document.querySelector('.popup_profile');
 export const popUpProfileSelector = '.popup_profile';
 export const popUpCloseBtn = document.querySelector('.popup__close');
 export const popUpForm = document.querySelector('.popup__form');
 export const nameInput = document.querySelector('.popup__input-text_type_name');
-export const jobInput = document.querySelector('.popup__input-text_type_job');
 
 // popup creat \\
 export const popUpCreat = document.querySelector('.popup-creat');
@@ -83,3 +83,15 @@ export const popImage = document.querySelector('.popup-img__image');
 export const popImageSelector = '.popup-img__image';
 export const popImageSub = document.querySelector('.popup-img__subtitle');
 export const popImageSubSelector = '.popup-img__subtitle';
+
+// popup remove 
+export const popupRemove = document.querySelector('.popup-remove');
+export const popupRemoveCloseBtn = document.querySelector('.popup-remove__close');
+export const popupRemoveForm = document.querySelector('.popup-remove__form');
+export const popupRemoveSubmit = document.querySelector('.popup-remove__button');
+
+//popup avatar
+export const popupAvatar = document.querySelector('.popup-avatar')
+export const popupAvatarContainer = document.querySelector('.popup-avatar__container');
+export const popupAvatarInput = document.querySelector('.popup-avatar__input-link');
+export const popupAvatarBtnSubmit = document.querySelector('.popup-avatar__button');
